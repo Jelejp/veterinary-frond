6c1cbcd866068c49783c983e529f92253f61df99
Jelena Palavecino <jelenapalavecino35@gmail.com>
2024-06-21 11:05:36 -0300

    utils

---
+++ src/utils/appointmentsList.js
@@ -0,0 +1,9 @@
+// Lista de turnos disponibles
+export const appointments = [
+    { id: 1, date: '2024-06-21', time: '10:00 AM' },
+    { id: 2, date: '2024-06-21', time: '11:00 AM' },
+    { id: 3, date: '2024-06-21', time: '01:00 PM' },
+    { id: 4, date: '2024-06-21', time: '02:00 PM' },
+    { id: 5, date: '2024-06-22', time: '10:00 AM' },
+    { id: 6, date: '2024-06-22', time: '11:00 AM' },
+];
