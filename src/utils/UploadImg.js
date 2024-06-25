990bb950bf710302b0bcb3be2a477f5814e3afc1
Leonelborjas <leonelborjas@outlook.es>
2024-06-25 16:33:42 -0300

    cambios

---
+++ src/utils/UploadImg.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Cloudinary } from '@cloudinary/url-gen';
+import { auto } from '@cloudinary/url-gen/actions/resize';
+import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
+import { AdvancedImage } from '@cloudinary/react';
+
+const UploadImg = () => {
+    const cld = new Cloudinary({ cloud: { cloudName: 'dmioftmku' } })
+    // Use this sample image or upload your own via the Media Explorer
+    const img = cld
+        .image('cld-sample-5')
+        .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
+        .quality('auto')
+        .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+
+    return (<AdvancedImage cldImg={img} />);
+};
+
+
+
+
+export default UploadImg
