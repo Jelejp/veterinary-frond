839bd1f764a7f60197c52a9b3eb55ec27e49b729
Jelena Palavecino <jelenapalavecino35@gmail.com>
2024-06-26 13:44:04 -0300

    arreglo de estilos

--- src/components/FooterAuth.jsx
+++ src/components/FooterAuth.jsx
@@ -4,7 +4,7 @@ import SocialMedia from './SocialMedia'
 const FooterAuth = () => {
     return (
         <>
-            <footer className="flex gap-3 bg-[#8BA8C4] font-bold text-white justify-around items-center flex-col md:flex-row lg:flex-row">
+            <footer className="flex gap-3 bg-[#8BA8C4] font-bold text-white justify-around items-center flex-col md:flex-row md:h-[15vh]">
                 <small className="text-xs">Veterinary clinic</small>
                 <small className="text-xs"> &#169; 2024 VetNova. All rights reserved.</small>
                 <SocialMedia />
