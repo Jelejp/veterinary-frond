c7c940e8d416236ed02a0181346e486ff02d042e
Jelena Palavecino <jelenapalavecino35@gmail.com>
2024-07-01 13:29:55 -0300

    create vet boton

--- src/components/NewVeterinarian.jsx
+++ src/components/NewVeterinarian.jsx
@@ -152,7 +152,7 @@ const NewVeterinarian = () => {
           type="submit"
           className="inline-flex items-center px-4 py-2 mt-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          Crear Veterinario
+          Create Veterinario
         </button>
       </div>
     </form>
